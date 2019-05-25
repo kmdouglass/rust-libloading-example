9e9dbeb04739c9507626239b204376137a2333f7
Kyle M. Douglass <kyle.m.douglass@gmail.com>
2019-05-25 13:02:50 +0200

    The Plugin struct now holds a Rc pointer to the object

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 use libc::c_int;
 use libloading::{Library, Symbol};
 
@@ -11,13 +13,13 @@ type SetInfo = extern "C" fn(*mut Object, c_int);
 
 struct Plugin<'a> {
     get_info: Symbol<'a, GetInfo>,
-    library: Library,
+    library: Rc<Library>,
     object: *mut Object,
     set_info: Symbol<'a, SetInfo>,
 }
 
 fn main() {
-    let library = Library::new("ffi-test/libffi-test.so").unwrap();
+    let library = Rc::new(Library::new("ffi-test/libffi-test.so").unwrap());
     let init: Symbol<Init> = unsafe { library.get(b"init\0").unwrap() };
 
     let object: *mut Object = init();
@@ -27,13 +29,13 @@ fn main() {
 
     let plugin = Plugin {
         get_info: get_info,
-        library: library,
+        library: Rc::clone(&library),
         object: object,
         set_info: set_info,
     };
 
-    println!("Original value: {}", (*plugin.get_info)(plugin.object));
-    (*plugin.set_info)(plugin.object, 42);
-    
-    println!("New value: {}", (*plugin.get_info)(plugin.object));
+    println!("Original value: {}", (plugin.get_info)(plugin.object));
+    (plugin.set_info)(plugin.object, 42);
+
+    println!("New value: {}", (plugin.get_info)(plugin.object));
 }
